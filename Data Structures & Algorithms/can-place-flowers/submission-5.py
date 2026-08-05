2e9e9be4a293069a7f247848ca8a6fb4db1315ba
Chanpitou Um <128171899+Chanpitou@users.noreply.github.com>
2026-08-05 13:02:16 -0500

    Add: can-place-flowers - submission-5

---
+++ Data Structures & Algorithms/can-place-flowers/submission-5.py
@@ -0,0 +1,16 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        if n == 0:
+            return True
+
+        m = len(flowerbed)  
+        for i in range(m):
+            left = (i == 0) or (flowerbed[i-1]==0)
+            right = (i==m-1) or (flowerbed[i+1]==0)
+
+            if flowerbed[i] == 0 and left and right:
+                flowerbed[i] = 1
+                n -= 1
+            if n == 0:
+                return True
+        return n==0
